4e375549b20211bf366c07735f116c364648d8e6
Valenhater <xnitezl@gmail.com>
2024-02-27 13:02:17 +0100

    Add project files.

---
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        int numero = 1;
+        public IActionResult Index()
+        {
+            this.numero += 1;
+            ViewData["NUMERO"] = "El valor del numero es " + this.numero;
+            return View();
+        }
+
+        //ACTION PARA VISUALIZAR DATOS EN SESION DE FORMA SIMPLE
+        public IActionResult SessionSimple(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    //Guardamos datos en session
+                    HttpContext.Session.SetString("nombre", "Programeitor");
+                    HttpContext.Session.SetString("hora", DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "Datos almacenados correctamente";
+
+                }else if (accion.ToLower() == "mostrar")
+                {
+                    ViewData["USUARIO"] = HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] = HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ MvcCoreSession/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//necesitamos habilitar el servicio de memoria cache
+builder.Services.AddDistributedMemoryCache();
+
+//session poidemos configurarlo con tiempo 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+//IMPORTANTE PONER AQUI EL SESSION EN ESTE ORDEN
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
